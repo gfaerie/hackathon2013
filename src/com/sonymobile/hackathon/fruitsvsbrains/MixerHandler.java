0a25e579cb5503eecc99e65797637a447e029391
Anders Westrup <anders@westrup.se>
2013-06-26 19:08:24 +0200

    braineaterhandler

--- src/com/sonymobile/hackathon/fruitsvsbrains/MixerHandler.java
+++ src/com/sonymobile/hackathon/fruitsvsbrains/MixerHandler.java
@@ -1,15 +1,12 @@
 package com.sonymobile.hackathon.fruitsvsbrains;
 
 public class MixerHandler {
-	private GameObject getMixer(GameState gameState) {
+	public void handleMixer(GameState gameState) {
+		GameObject mixer = null;
 		for (GameObject gameObject : gameState.getObjects()) {
 			if (gameObject.getType() == GameObjectType.TARGET_CONTAINER)
-				return gameObject;
+				mixer = gameObject;
 		}
-		return null;
-	}
-	public void handleMixer(GameState gameState) {
-		GameObject mixer = getMixer(gameState);
 		if (mixer == null)
 			return;
 		for (GameObject gameObject : gameState.getObjects()) {
