3994be734b4135f7f7ad8de8da6e884a69a732ba
Mns Schultz <mans.schultz@sonymobile.com>
2013-06-26 16:15:33 +0200

    Started on movement

--- src/com/sonymobile/hackathon/fruitsvsbrains/GameGraphics.java
+++ src/com/sonymobile/hackathon/fruitsvsbrains/GameGraphics.java
@@ -1,5 +1,17 @@
 package com.sonymobile.hackathon.fruitsvsbrains;
 
 public enum GameGraphics {
-	APPLE,BRAIN,MIXER;
+	APPLE(25),BRAIN(30),MIXER(100);
+	
+	private int size;
+
+	private GameGraphics(int size) {
+		this.size = size;
+	}
+
+	public int getSize() {
+		return size;
+	}
+	
+	
 }
